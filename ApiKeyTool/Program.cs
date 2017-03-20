7471c07576f575935507694bb931b474b69b233c
jianpengh <jianpengh@tujia.com>
2017-03-20 09:45:54 +0800

    Add project files.

---
+++ ApiKeyTool/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiKeyTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入密码明文:");
+            var inputString = Console.ReadLine();
+            var password = TuJia.Framework.Utility.Encrypt.Md5Hash(inputString);
+            Console.WriteLine(password);
+            System.Diagnostics.Trace.WriteLine(password);
+            Console.ReadLine();
+        }
+    }
+}
